1cc79e46a0b14728e7d0eabde64d4c1e8b140d12
Aditya Galang Adzana <adityagalang80@gmail.com>
2023-10-21 00:38:56 +0700

    Add Scanner Object with content

--- Assets/Script/PopupObjectMarker.cs
+++ Assets/Script/PopupObjectMarker.cs
@@ -7,25 +7,54 @@ using UnityEngine.UI;
 
 public class PopupObjectMarker : MonoBehaviour
 {
-    public List<PopupObjectSO> ObjectDataMarker;
+    public List<DataPopup> DataObjectPopup = new List<DataPopup>();
     public RawImage PopupImage;
     public TextMeshProUGUI PopupTitle;
     public TextMeshProUGUI PopupContent;
     public ScrollRect ScrollView;
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ShowPopupData(ObjectLasID.regulator);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            ShowPopupData(ObjectLasID.weldinggun);
+        }
+    }
     public void ShowPopupData(ObjectLasID SelectedID)
     {
-        PopupObjectSO data = ObjectDataMarker.Find(currentID => currentID.ID == SelectedID);
-        PopupImage.texture = data.PopupTexture;
-        PopupTitle.text = data.PopupTitle;
-        PopupContent.text = data.PopupText.ToString();
+        ShowContent(SelectedID);
         this.gameObject.SetActive(false);
         this.gameObject.SetActive(true);
         ScrollView.verticalNormalizedPosition = 1;
     }
-
+    public void ShowContent(ObjectLasID ID)
+    {
+        foreach(DataPopup data in DataObjectPopup)
+        {
+            if(data.ID == ID)
+            {
+                data.PopupGame.SetActive(true);
+            }
+            else
+            {
+                data.PopupGame.SetActive(false);
+            }
+        }
+    }
+    
     public void ClosePopup()
     {
         this.gameObject.SetActive(false);
+    }
+
+    [System.Serializable]
+    public struct DataPopup
+    {
+        public ObjectLasID ID;
+        public GameObject PopupGame;
     }
 }
